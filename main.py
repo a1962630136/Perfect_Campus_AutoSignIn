24036281f122e8477a8cf8e780518f4f28137c58
a1962630136 <55832644+a1962630136@users.noreply.github.com>
2020-12-29 11:50:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def GetUserJson(deptId,text,stuNum,userName,phone,guardianPhone,userId,customerI
 	"jsonData": {
 		"deptStr": {
 			"deptid": 62420,
-			"text": Text
+			"text": "教职工-公共课教学部-默认"
 		},
 		"areaStr": {"streetNumber":"","street":"新七大道","district":"浉河区","city":"信阳市","province":"河南省","town":"","pois":"信阳师范学院","lng":114.04613300000214 + random.random()/100000,"lat":32.15061303752275 + random.random()/100000,"address":"浉河区新七大道信阳师范学院","text":"河南省-信阳市","code":""},
 		"reportdate": round(time.time()*1000),
@@ -107,7 +107,7 @@ def GetUserJson(deptId,text,stuNum,userName,phone,guardianPhone,userId,customerI
 		"source": "app",
 		"templateid": "pneumonia",
 		"stuNo": 202000178,
-		"username": 邹铮铮,
+		"username": "邹铮铮",
 		"phonenum":18120272067,
 		"userid": 19942552,
 		"updatainfo": [{
